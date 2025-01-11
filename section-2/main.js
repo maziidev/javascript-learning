c56c2f24d425b30035e68a3f3771329de4d16793
maziidev <godswillnathaniel97@gmail.com>
2025-01-11 23:30:06 +0100

    finish some of the important basics of javascript sha

---
+++ section-2/main.js
@@ -0,0 +1,104 @@
+// VARIABLE
+
+/*
+// let age = 25;
+// console.log(age);
+
+// age = 30;
+// console.log(age);
+
+// const userName = 'mazii';
+// console.log(userName);
+
+// older keyword depreciated
+// var score = 75;
+// console.log(score);
+// var score = 20;
+// console.log(score);
+*/
+
+// ====================================STRINGS STARTS HERE==================================
+/*
+console.log('Hello World');
+
+// concatenation of strings
+
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[0]);
+
+// string length
+
+console.log(fullName.length);
+
+// string methods
+// methods function associated with a particular object, or datatype
+
+console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+let result = fullName.toLowerCase();
+
+console.log(result);
+
+
+// Common string methods
+
+let email = 'learning@gmail.com';
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(0, 10);
+
+// let result = email.substr(4, 10);
+
+// let result = email.replace('m', 'w');
+
+console.log(result);
+*/
+
+
+// ====================================STRINGS END HERE==================================
+
+// ====================================Numbers start HERE==================================
+
+/*
+
+// math operators +, -, /, *, **,%
+
+let radius = 10;
+const pi = 3.14;
+*/
+
+// ====================================Numbers end HERE==================================
+
+
+// ====================================array start HERE==================================
+
+/*
+
+let ninjas = ['Godswill', 'Mazii', 'Patrick'];
+let ages = [20, 29, 30, 60];
+
+let random = ['shaun', 'crystal', 30, 20];
+console.log(ninjas[1]);
+let ninjas = ['Godswill', 'Mazii', 'Patrick'];
+
+// array method
+// let result = ninjas.join('-');
+// let result = ninjas.indexOf('Mazii');
+
+// let result = ninjas.concat(['ken', 'dindu']);
+
+// let result = ninjas.push('ken');
+
+console.log(result);
+*/
+
+
+
+// ====================================array ends HERE==================================
